a2893863453ecdb8db455d518de44135d95f9338
santosh7117 <075bct081.santosh@pcampus.edu.np>
2021-06-21 06:19:56 +0545

    Revert "Revert "Initial commit""
    This reverts commit 0fe3da11515d4bf8c4db4376b9e4600999459b9e.

---
+++ Lab 2/OR.m
@@ -0,0 +1,5 @@
+function bool = OR(b1, b2)
+if (b1 == b2 && b1 ==0) bool = 0;
+else bool = 1;
+endif
+endfunction
---
+++ Lab 3/NOT.m
@@ -0,0 +1,5 @@
+function cmpl = NOT(b)
+	if (b >= 1) cmpl = 0;
+	else cmpl = 1;
+	endif
+endfunction
